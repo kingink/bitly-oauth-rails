36a542fe327a0372d13fda3f625b841d25989ec1
KingInk <kfir.shay@gmail.com>
2013-06-23 17:47:33 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BitlyOauthRailsExample::Application.config.secret_key_base = '5c7e4be2a0466b20dcd25c5549a3b105d7f34fda8db6c25d594a9b4beb618b435afe934303f987f5c006740a44f6b277b906f440cae9399f190fc81bd9db97a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BitlyOauthRailsExample::Application.config.session_store :cookie_store, key: '_bitly-oauth-rails-example_session'
